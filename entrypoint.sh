737af9541951bcd383fd413c745ac2f832eac757
Michiel van Beers <michiel.van.beers@plat4mation.com>
2022-10-11 21:18:53 +0200

    Fix crontab minute

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,8 @@ then
     python ocr.py
 else
     echo "Found scan frequency variable, adding crontab"
-    (crontab -l 2>/dev/null; echo "*/$SCAN_FREQUENCY * * * * python ocr.py") | crontab -
-    
+    crontab -l | { cat; echo "*/$SCAN_FREQUENCY * * * * python ocr.py"; } | crontab -
+        
     echo "Scanning every $SCAN_FREQUENCY minute(s)"
     tail -f
 fi
-
-
-
-
