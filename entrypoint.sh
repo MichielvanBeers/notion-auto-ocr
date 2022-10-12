18149ffe6898a35fffe28369b42ecf5f37b1be9a
Michiel van Beers <michiel.van.beers@plat4mation.com>
2022-10-12 09:02:15 +0200

    Write env variables to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Writing environment variables to /etc/environment"
+printenv | grep -v "no_proxy" >> /etc/environment
+
 if [ -z "$SCAN_FREQUENCY" ] 
 then
     echo "Running single instance of scan"
@@ -9,5 +12,5 @@ else
     (crontab -l 2>/dev/null; echo "*/$SCAN_FREQUENCY * * * * python /app/ocr.py &>/output.log") | crontab -
 
     echo "Scanning every $SCAN_FREQUENCY minute(s)"
-    tail -f
+    cron -f
 fi
