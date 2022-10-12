476f5f25541846602624e7d95c81aa9ab6a874d1
Michiel van Beers <michiel.van.beers@plat4mation.com>
2022-10-12 11:42:11 +0200

    Test crontab install

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ then
     echo "Running single instance of scan"
     python ocr.py
 else
-    echo "Found scan frequency variable, adding crontab"
-    (crontab -l 2>/dev/null; echo "*/$SCAN_FREQUENCY * * * * /usr/local/bin/python ocr.py > output.log") | crontab -
+    #echo "Found scan frequency variable, adding crontab"
+    #(crontab -l 2>/dev/null; echo "*/$SCAN_FREQUENCY * * * * /usr/local/bin/python ocr.py > output.log") | crontab -
+    echo "Testing crontab"
+    (crontab -l 2>/dev/null; echo "* * * * * date >/proc/1/fd/1 2>/proc/1/fd/2") | crontab -
+    
 
     echo "Scanning every $SCAN_FREQUENCY minute(s)"
     service cron start & tail -f output.log
